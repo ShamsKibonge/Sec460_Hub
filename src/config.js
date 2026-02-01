de801a893635a1af403bdee60d19ec5f57d7a64e
ShamsKibonge <104319361+ShamsKibonge@users.noreply.github.com>
2026-02-01 12:47:24 -0800

    Fix routing (HashRouter) and switch to HTTPS

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = "http://localhost:5000"; // for local dev if not using proxy
-export const API_BASE_URL = "http://sec460.sofkam.com:80";
+export const API_BASE_URL = "https://sec460.sofkam.com";
